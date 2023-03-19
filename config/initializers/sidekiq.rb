d3a075a5bba1933ca3ad0b586410850ce0b95373
CBL <cyril@dilolabs.fr>
2023-03-19 16:48:08 +0100

    Fix Sidekiq for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,7 @@
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0'), namespace: "sidekiq-#{Rails.env}" }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') }
+end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0'), namespace: "sidekiq-#{Rails.env}" }
-  end
-else
-  Sidekiq.configure_server do |config|
-    config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') }
-  end
+Sidekiq.configure_client do |config|
+  config.redis = { url: (ENV["REDIS_URL"] || 'redis://localhost:6379/0') }
 end
